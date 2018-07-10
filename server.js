5e4fa05788fe7055fae70007f378a1796d4d9f39
Kyle Roberts <kylewr@tm7.tech>
2018-07-10 14:21:33 -0400

    fixed typos, added content, replaced logo

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const data = {
     data:{
         slides:[
             {white:false,image_id:1,header_s:'We\'re TM7',symbol:'',text:'A modern team with big dreams',header_e:''},
-            {white:true,image_id:2,header_s:'We',symbol:'heart',text:'Angular,Java,MongoDB,ESB',header_e:'Agile'},
-            {white:true,image_id:3,header_s:'We',symbol:'heart',text:'Angular,Java,MongoDB,ESB',header_e:'Agile'}
+            {white:true,image_id:2,header_s:'We',symbol:'heart',text:'FinTech, InsurTech, RegTech',header_e:'Agile'},
+            {white:true,image_id:3,header_s:'We',symbol:'heart',text:'Web, Bots, AI, Blockchain',header_e:'Agile'}
        
         ],
         navLinks: 
